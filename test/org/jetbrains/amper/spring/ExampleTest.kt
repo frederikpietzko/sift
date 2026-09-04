94cca919ac26a4309ab36931d046dfc9fdffbcfd
frederikpietzko <frederik.pietzko@jetbrains.com>
2026-09-04 22:45:55 +0200

    initial commit

---
+++ test/org/jetbrains/amper/spring/ExampleTest.kt
@@ -0,0 +1,13 @@
+package org.jetbrains.amper.spring
+
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Test
+
+@SpringBootTest
+class ExampleTest {
+
+    @Test
+    fun contextLoads() {
+
+    }
+}
